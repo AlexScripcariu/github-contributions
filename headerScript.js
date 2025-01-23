2053c2d0180187cf77862bd4374cc72a097582c5
Alex Scripcariu <alexscripcariu41209@gmail.com>
2025-01-23 06:42:26 +0000

    revamped header

--- headerScript.js
+++
@@ -1,9 +0,0 @@
-// const header = document.getElementsByTagName("header");
-
-const hamburgerIcon = document.querySelector(".hamburger-icon");
-console.log(hamburgerIcon);
-
-hamburgerIcon.addEventListener("click", () => {
-  hamburgerIcon.classList.toggle('hamburger-icon');
-  hamburgerIcon.classList.toggle('hamburger-icon-clicked');
-})
