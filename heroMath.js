c83b751f6b61045b13f1e8f82bd591de874b6178
AlexScripcariu <alexscripcariu41209@gmail.com>
2025-01-24 12:17:37 +0000

    ADDED ANGLE CALCUATOR

--- heroMath.js
+++ heroMath.js
@@ -1,25 +1,69 @@
 const heroSection = document.getElementById("hero");
 
-let prevX = 0;
-let prevY = 0;
+let xOne;
+let xTwo;
+let yOne;
+let yTwo;
+let dy;
+let dx;
+
 
 let isMouseDown = false;
 
 heroSection.onmousedown = (event) => {
   isMouseDown = true;
-  prevX = event.clientX;
-  prevY = event.clientY;
+  xOne = event.clientX;
+  yOne = event.clientY;
 }
 
-heroSection.onmousemove = (event) => {
+// heroSection.onmousemove = (event) => {
 
-  if (isMouseDown) {
-    console.log(`Mouse moving! X: ${event.clientX} | Y: ${event.clientY}`);
-  }
+//   if (isMouseDown) {
+//     console.log(`Mouse moving! X: ${event.clientX} | Y: ${event.clientY}`);
+//   }
   
+// }
+
+
+heroSection.onmouseup = (event) => {
+  xTwo = event.clientX;
+  yTwo = event.clientY;
+
+  calculateChange(xOne, xTwo, yOne, yTwo);
+}
+
+function calculateChange(x1, x2, y1, y2) {
+  dy = y2 - y1;
+  dx = x2 - x1;
+  console.log(`DY: ${dy} DX: ${dx}`);
+  calculateAngle(dy, dx);
 }
 
 
-heroSection.onmouseup = () => {
-  isMouseDown = false;
+function calculateAngle(dyValue, dxValue) {
+  let opposite;
+  let adjacent;
+
+  if (dyValue === 0 && dxValue >= 0) {
+    return 90;
+  } else if (dyValue === 0 && dxValue <= 0) {
+    return -90;
+  } else if (dxValue === 0 && dyValue >= 0) {
+    return 0;
+  } else if (dxValue === 0 && dyValue <= 0) {
+    return 180;
+  } else if (dyValue > 0) {
+    opposite = dyValue;
+    adjacent = dxValue;
+  } else if (dyValue < 0) {
+    opposite = dxValue;
+    adjacent = dyValue;
+  } else {
+    return null;
+  }
+
+  const tanInv = Math.atan(opposite / adjacent) * (180 / Math.PI);
+  console.log(tanInv)
+  return tanInv;
 }
+
